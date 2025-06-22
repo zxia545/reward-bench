69bce341963642ff8ac9257c3d65f1f1cadf8dfd
zxia545 <xiaozheyuan72@gmail.com>
2025-06-22 20:43:56 +1200

    Add test rm

---
+++ test_rm.sh
@@ -0,0 +1,28 @@
+python scripts/run_rm.py \
+--model=/root/.cache/huggingface/hub/models--Qwen--Qwen2.5-0.5B-Instruct/snapshots/7ae557604adf67be50417f59c2c2f167def9a775 \
+--do_not_save
+
+# /root/.cache/huggingface/hub/models--Qwen--Qwen2.5-0.5B-Instruct/snapshots/7ae557604adf67be50417f59c2c2f167def9a775
+
+# {
+#     // Use IntelliSense to learn about possible attributes.
+#     // Hover to view descriptions of existing attributes.
+#     // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+#     "version": "0.2.0",
+#     "configurations": [
+#         {
+#             "name": "Test RM - Generative",
+#             "type": "debugpy",
+#             "request": "launch",
+#             "program": "${workspaceFolder}/scripts/run_rm.py",
+#             "console": "integratedTerminal",
+#             "args": [
+#                 "--model=/root/.cache/huggingface/hub/models--Qwen--Qwen2.5-0.5B-Instruct/snapshots/7ae557604adf67be50417f59c2c2f167def9a775",
+#                 "--do_not_save"
+#             ],
+#             "cwd": "${workspaceFolder}",
+#             "env": {},
+#             "justMyCode": false
+#         }
+#     ]
+# }
